5ec1f082c8414ec0bb8cb1d018482f7b5f948287
didof <didonato.fr@gmail.com>
2021-01-18 19:24:39 +0100

    using command name and description in introduction message

--- src/commands/introduce.js
+++ src/commands/introduce.js
@@ -9,10 +9,11 @@ module.exports = {
             .setAuthor('made with <3 by @didof')
             .setTitle(`Hello, my name is ${bot.user.username}`)
             .setDescription('I am a discord bot, made in node.js. I am still growing up.')
-            .addField('js:introduce', 'display this message')
-            .addField('js:help', 'display the help message')
             .setFooter('if at any time you want to know what I can do for you, call me with prefix: help')
 
+        Array.from(bot.commands.values()).forEach(function ({ name, description }) {
+            embed.addField(name, description)
+        })
         message.channel.send(embed)
     }
 }
